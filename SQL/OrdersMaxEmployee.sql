5b40e45e069b961e6c7bc0bbbdca2f316ad4ad5c
dancingkimDH <kimdonghyun0701@gmail.com>
2023-06-08 16:41:13 +0900

    update

--- SQL/OrdersMaxEmployee.sql
+++ SQL/OrdersMaxEmployee.sql
@@ -1,6 +1,6 @@
 -- Count값을 COU라 하자/최댓값은 어떻게?
 --방법 1
-SELECT Count(*) AS COU, EMP.LastName, EMP.FirstName
+SELECT Count(*) AS COU, EMP.Last_name, EMP.First_name
 FROM Orders AS ORD
 	INNER JOIN Employees AS EMP
     ON ORD.EmployeeID = EMP.EmployeeID
@@ -13,11 +13,11 @@ FROM Orders AS ORD
 
 --방법 2
 --ALL_COU(아래 SELECT문) 테이블(만들어진 임시테이블)의 COU의 최댓값, 그리고 나머지 값을 SELECT
-SELECT MAX(ALL_COU.COU) AS MAX_COU, ALL_COU.LAS, ALL_COU.FIR
+SELECT MAX(ALL_COU.COU) AS MAX_COU, ALL_COU.Last_name, ALL_COU.FIR
 FROM ( 
     --아래 SELECT문을 ALL_COU라 하자
     --AS를 이용해서 임시테이블화. 괄호로 감싸기.
-		SELECT ORD.EmployeeID, Count(ORD.EmployeeID) AS COU, EMP.LastName AS LAS, EMP.FirstName AS FIR
+		SELECT ORD.EmployeeID, Count(ORD.EmployeeID) AS COU, EMP.LastName AS Last_name, EMP.FirstName AS First_name
         FROM Orders AS ORD
         INNER JOIN Employees AS EMP
         ON ORD.EmployeeID = EMP.EmployeeID
