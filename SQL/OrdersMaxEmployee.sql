0770d8292e9f1fd4a2c0cf329d1e72c0fa626590
dancingkimDH <kimdonghyun0701@gmail.com>
2023-06-08 16:41:51 +0900

    update

--- SQL/OrdersMaxEmployee.sql
+++ SQL/OrdersMaxEmployee.sql
@@ -13,7 +13,7 @@ FROM Orders AS ORD
 
 --방법 2
 --ALL_COU(아래 SELECT문) 테이블(만들어진 임시테이블)의 COU의 최댓값, 그리고 나머지 값을 SELECT
-SELECT MAX(ALL_COU.COU) AS MAX_COU, ALL_COU.Last_name, ALL_COU.FIR
+SELECT MAX(ALL_COU.COU) AS MAX_COU, ALL_COU.Last_name, ALL_COU.First_name
 FROM ( 
     --아래 SELECT문을 ALL_COU라 하자
     --AS를 이용해서 임시테이블화. 괄호로 감싸기.
